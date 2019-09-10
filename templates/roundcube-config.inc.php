6bf46de3ef4655bb0248cf6973a5c3c1b5c8f4ec
anto2oo <antonin.guyot75@gmail.com>
2019-09-10 17:22:44 +0200

    initial commit

---
+++ templates/roundcube-config.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+Below this line is the default config
+file for furnace that has been
+automatically added to this file.
+*/
+
+$config['db_dsnw'] = 'mysql://roundcube_user:${mysql_roundcube_passwd}@localhost/roundcube_db';
+
+$config['default_host'] = 'ssl://localhost';
+
+$config['support_url'] = '';
+
+$config['identities_level'] = 3;
+
+$config['smtp_server'] = 'localhost';
+
+$mail_config['smtp_user'] = '%u';
+
+$mail_config['smtp_pass'] = '%p';
+
+$config['quota_zero_as_unlimited'] = true;
+
+$config['preview_pane'] = true;
+
+$config['read_when_deleted'] = false;
+
+$config['check_all_folders'] = true;
