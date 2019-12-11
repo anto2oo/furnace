215e5d8346fca68dc806a32d1f319c1eba49d600
Antonin <antonin.guyot@icloud.com>
2019-12-11 20:58:23 +0100

    cleaner parameters input

--- templates/api/forward.php
+++ templates/api/forward.php
@@ -5,10 +5,25 @@
  * @param string $command
  * @return array|mixed
  */
-function run($command = ''){
+function run($command = '', $parameters = ''){
+    $query = 'sudo /usr/local/bin/furnace ' . $command . ' ' . $parameters;
+
     $output = [];
-    exec('sudo /usr/local/bin/furnace ' . $command, $output);
+    exec($query, $output);
     return count($output) === 1 ? $output[0] : $output;
+}
+
+/**
+ * Parses arguments from array to bash-syntax
+ * @param $arguments
+ * @return string
+ */
+function parse_args($arguments){
+    $parsed = '';
+    foreach ($arguments as $key => $value) {
+        $parsed .= '--' . $key . ' "' . $value . '" ';
+    }
+    return $parsed;
 }
 
 // Base settings
@@ -16,11 +31,13 @@ $app_token = run('api token');
 
 // Passed data
 $token = $_GET['token'];
+unset($_GET['token']);
 $command = $_GET['cmd'];
+unset($_GET['cmd']);
 
 if($token !== $app_token){
     die('Invalid token');
 }
 
 header('Content-type: application/json');
-echo json_encode(run($command));
+echo json_encode(run($command, parse_args($_GET)));
